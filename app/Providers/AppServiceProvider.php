7ff846d64ffac947c9ee57a6846fbeb3f3e16bab
yyunda774-web <yyunda774@gmail.com>
2026-03-26 11:23:42 +0700

    fix https force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       if (env('APP_ENV') === 'production') {
+        // Paksa semua URL jadi HTTPS
         URL::forceScheme('https');
-       }
     }
 }
